9a3d243883bf036ec5696f33a30b0c677b56c1cc
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-05-19 23:49:55 +0300

    deneme

--- day-story-web/src/App.js
+++ day-story-web/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import '/node_modules/@material/web/all.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+<md-outlined-text-field label="Label" value="Value">
+</md-outlined-text-field>
+
     </div>
   );
 }
