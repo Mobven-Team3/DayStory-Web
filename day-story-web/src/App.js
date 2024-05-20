4a18619dce2913f50fdc9ba324a0f4d708267fad
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-05-20 15:21:20 +0300

    klasör

--- day-story-web/src/App.js
+++ day-story-web/src/App.js
@@ -1,16 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import ReceptionPage from './Pages/ReceptionPage.jsx';
 import '/node_modules/@material/web/all.js';
+
 
 function App() {
   return (
     <div className="App">
-
-
-<md-outlined-text-field label="Label" value="Value">
-</md-outlined-text-field>
-
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<ReceptionPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
 
+}
 export default App;
---
+++ day-story-web/src/Pages/ReceptionPage/ReceptionPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReceptionPage = () => {
+  return <div>aysu</div>;
+};
+
+export default ReceptionPage;
