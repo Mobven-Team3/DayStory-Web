547c7e95efbb3c3cd6c6639a1cba06310a649a92
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-05-28 20:32:22 +0300

    buton ismi

--- day-story-web/src/Pages/ReceptionPage/ReceptionPage.jsx
+++ day-story-web/src/Pages/ReceptionPage/ReceptionPage.jsx
@@ -12,13 +12,17 @@ const ReceptionPage = () => {
     const handleRegisterClick = () => {
         navigate('/register'); // 'register' rotasına yönlendirme yapar
     };
-
+    const handleMainClick = () => {
+        navigate('/mainpage'); // 'register' rotasına yönlendirme yapar
+    };
 
 
     return (
         <>
             <button onClick={handleRegisterClick} > kayıt ol</button>
             <button onClick={handleLoginClick}> giriş Yap</button>
+            <button onClick={handleMainClick}> Main Page</button>
+
         </>
     );
 }
