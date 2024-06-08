a1e452ec80c28972ba59ccf10301bdf964c07d6e
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-06-08 22:46:45 +0300

    scssler

--- src/Pages/GalleryPage/GalleryDetailPage.jsx
+++ src/Pages/GalleryPage/GalleryDetailPage.jsx
@@ -71,6 +71,9 @@
 
 // export default GalleryDetailPage;
 
+
+
+
 // import axios from 'axios';
 // import React, { useEffect, useState } from 'react';
 // import { useParams } from 'react-router-dom';
