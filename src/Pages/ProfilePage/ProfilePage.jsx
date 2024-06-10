dcd58428643dcdb653cfa6295964ed62f5860a2c
S-NujenDemirr <nujenndemirr@gmail.com>
2024-06-10 11:46:07 +0300

    deneme

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import '../NotePage/note-scss/_note.scss';
-
-// images
-
-
-// icons
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Card, CardContent, Typography, Avatar, Grid } from '@mui/material';
+import NavigationBar from '../../../src/Pages/Navbar/Navbar';
 
 
-const ProfilePage = () => {
 
+const Profile = () => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
 
-    return (
-        <>
-            <div className='content'>
-            </div>
-        </>
-    );
+  useEffect(() => {
+    const token = localStorage.getItem('token'); // Token'ı localStorage'dan al
+    if (!token) {
+      setError('Token not found');
+      return;
+    }
+
+    axios.get('http://165.22.93.225:5003/api/Users/', {
+      headers: {
+        Authorization: Bearer ${token}
+      }
+    })
+    .then(response => {
+      setUser(response.data);
+    })
+    .catch(error => {
+      setError('User not found');
+    });
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Card style={{ width: '60%', padding: '20px' }}>
+        <CardContent>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12} sm={4}>
+              <Avatar alt={user.firstName} src="/static/images/avatar/1.jpg" style={{ width: '150px', height: '150px' }} />
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <Typography variant="h5">{${user.firstName} ${user.lastName}}</Typography>
+              <Typography variant="body1">Username: {user.username}</Typography>
+              <Typography variant="body1">Email: {user.email}</Typography>
+              <Typography variant="body1">Birth Date: {user.birthDate}</Typography>
+              <Typography variant="body1">Gender: {user.gender}</Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
-export default ProfilePage;
-
-
-
-
-
+export default Profile;
