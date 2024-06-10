d7c0f75769a914bfa505d352f362f074a61fb465
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-06-10 13:46:28 +0300

    main

--- src/Pages/GalleryPage/GalleryDetailPage.jsx
+++ src/Pages/GalleryPage/GalleryDetailPage.jsx
@@ -221,7 +221,7 @@ const GalleryDetailPage = () => {
         const fetchEvents = async () => {
             try {
                 const token = localStorage.getItem('token');
-                const response = await axios.get('https://165.22.93.225:5043/api/Events/day', {
+                const response = await axios.get('https://talent.mobven.com:5043/api/Events/day', {
                     params: { date },
                     headers: {
                         'Authorization': `Bearer ${token}`,
