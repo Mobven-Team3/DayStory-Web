631b68750f9b4416b69b695e77fec75d99bd7207
S-NujenDemirr <nujenndemirr@gmail.com>
2024-06-11 23:24:00 +0300

    gallery sayfası yorum satırlarını sildim

--- src/Pages/GalleryPage/GalleryPage.jsx
+++ src/Pages/GalleryPage/GalleryPage.jsx
@@ -149,6 +149,3 @@ const GalleryPage = () => {
 };
 
 export default GalleryPage;
-
-
-
--- src/Pages/NotePage/notedetail.jsx
+++ src/Pages/NotePage/notedetail.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './gallery-scss/_gallery-detail.scss';
+import '../GalleryPage/gallery-scss/_gallery-detail.scss';
 
 import loadingimg from '../../assets/images/loading.png';
 
