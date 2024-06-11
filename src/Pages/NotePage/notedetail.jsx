a67180aab1e1521fc3def8fa92327b281694ea48
S-NujenDemirr <nujenndemirr@gmail.com>
2024-06-11 17:27:51 +0300

    api notePage

---
+++ src/Pages/NotePage/notedetail.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Typography, Box } from '@mui/material';
+import NavigationBar from '../../../src/Pages/Navbar/Navbar';
+import "./note-scss/_note.scss";
